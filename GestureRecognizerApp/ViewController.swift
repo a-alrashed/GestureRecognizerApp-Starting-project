b6c850abad370270fa3dc9285771e003d5b05acb
Azzam R Alrashed <a.alrashed@safcsp.org.sa>
2020-12-13 11:31:15 +0300

    Initial Commit

---
+++ GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Azzam R Alrashed on 13/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
